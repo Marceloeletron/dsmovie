6ecb8d90994953d7a29f6742613c5e954d55bc7c
Marcelo Ribeiro <marceloleitao1893@gmail.com>
2022-01-14 05:13:38 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/dvsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,44 @@
+package com.dvsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	
+	private Double value;
+	
+	public Score() {
+	}
+	//Serve para informar o filme e associar ao meu Score
+	public void  setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void serUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
+}
